861ef4bf06dbfefbec26adeec65e9ea52fafe2b3
romi615 <romimawandia615@gmail.com>
2025-12-19 13:12:36 +0530

    added new components and modified in home

--- client/src/pages/Home.jsx
+++ client/src/pages/Home.jsx
@@ -1,12 +1,15 @@
 import React from "react";
 import Hero from "../components/Hero";
+import FeaturedSection from "../components/FeaturedSection";
 
 function Home() {
   return (
     <>
       <Hero />
+      <FeaturedSection />
     </>
   );
 }
 
 export default Home;
+  
