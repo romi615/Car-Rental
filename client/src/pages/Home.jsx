903eba1e0cbe116da7e4ec381c9843310c02a0cc
romi615 <romimawandia615@gmail.com>
2025-12-20 10:52:46 +0530

    added Footer, newsletter,and testmonials. banner, app something is cmodified

--- client/src/pages/Home.jsx
+++ client/src/pages/Home.jsx
@@ -1,12 +1,18 @@
 import React from "react";
 import Hero from "../components/Hero";
 import FeaturedSection from "../components/FeaturedSection";
+import Banner from "../components/Banner";
+import Testimonial from "../components/Testimonial";
+import Newsletter from "../components/Newsletter";
 
 function Home() {
   return (
     <>
       <Hero />
       <FeaturedSection />
+      <Banner />
+      <Testimonial />
+      <Newsletter/>
     </>
   );
 }
